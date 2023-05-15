0ffa0083b7ff6488a13248f8fb98b1dd77d7ee8a
Jaykishan <uchiha.jay007@gmail.com>
2023-05-15 17:02:57 +0530

    Error handling class for zetsu node service.

--- src/zetsu/src/index.ts
+++ src/zetsu/src/index.ts
@@ -3,6 +3,8 @@ import connectDB from './database';
 import { healthRouter } from './routes/api/health';
 import morganMiddleware from './middleware/http-log';
 import logger from './lib/logger';
+import { errorHandler } from './middleware/error-handler';
+import { DatabaseConnectionError } from './errors/database-connection-error';
 
 const app = express();
 const port = 3010;
@@ -11,10 +13,17 @@ connectDB();
 app.use(morganMiddleware);
 app.use('/health', healthRouter);
 
-app.get('/api/zetsu/', (_req, res) => {
+app.get('/api/zetsu/', (req, res) => {
   logger.debug('debug info');
   res.send('Zetsu V1');
 });
+
+app.get('/api/zetsu/error', (req, res) => {
+  logger.error('database error');
+  throw new DatabaseConnectionError();
+});
+
+app.use(errorHandler);
 
 app.listen(port, () => {
   logger.info(`Server running at http://localhost:${port}`);
