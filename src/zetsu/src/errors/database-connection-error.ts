d1b4c2f2de12eca4527dac422cce418b7e45b795
Jaykishan <uchiha.jay007@gmail.com>
2023-05-16 10:50:03 +0530

    removal of text duplication from database connection error ts file.

--- src/zetsu/src/errors/database-connection-error.ts
+++ src/zetsu/src/errors/database-connection-error.ts
@@ -2,10 +2,9 @@ import { CustomError, IErrorStruct } from './custom-error';
 
 export class DatabaseConnectionError extends CustomError {
   statusCode = 500;
-  reason = 'Error connecting to database';
 
-  constructor() {
-    super('Error connecting to database');
+  constructor(public reason: string = 'Error connecting to database') {
+    super(reason);
 
     Object.setPrototypeOf(this, DatabaseConnectionError.prototype);
   }
