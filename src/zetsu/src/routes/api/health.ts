d4b29e6d37f3442f28500c1107015fe2092816a0
Jaykishan <uchiha.jay007@gmail.com>
2023-05-19 11:28:03 +0530

    renaming req variables.

--- src/zetsu/src/routes/api/health.ts
+++ src/zetsu/src/routes/api/health.ts
@@ -4,7 +4,7 @@ import { logger } from '@cashoco/common';
 
 const router: Router = Router();
 
-router.get('/ready', async (_req: Request, res: Response) => {
+router.get('/ready', async (req: Request, res: Response) => {
   logger.info('Checking the readiness of the application');
   try {
     // Check if the MongoDB connection is established and ready
@@ -20,7 +20,7 @@ router.get('/ready', async (_req: Request, res: Response) => {
   }
 });
 
-router.get('/live', (_req, res) => {
+router.get('/live', (req, res) => {
   logger.info('Checking the liveness of the application');
   res.status(200).json({ message: 'Application is live' });
 });
