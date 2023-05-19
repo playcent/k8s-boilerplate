87deb7b9998fa4636fbd66bacd2cd694e694b13d
Jaykishan <uchiha.jay007@gmail.com>
2023-05-19 11:30:20 +0530

    refactored indra service to use common module.

--- src/indra/src/index.ts
+++ src/indra/src/index.ts
@@ -1,15 +1,22 @@
 import express from 'express';
-import logger from './lib/logger';
-import morganMiddleware from './middleware/http-log';
+import { morganMiddleware, logger, errorHandler, DatabaseConnectionError } from '@cashoco/common';
 
 const app = express();
 const port = 3000;
 
 app.use(morganMiddleware);
-app.get('/api/indra/', (_req, res) => {
+
+app.get('/api/indra/', (req, res) => {
   logger.debug('debug info');
   res.send('Indra V1');
 });
+
+app.get('/api/indra/error/', (req, res) => {
+  logger.error('database error');
+  throw new DatabaseConnectionError();
+});
+
+app.use(errorHandler);
 
 app.listen(port, () => {
   logger.info(`Server running att http://localhost:${port}`);
